3c8583301ad663b1200e2e26316959b1d894f134
dsr0010 <63587300+dsr0010@users.noreply.github.com>
2023-09-01 09:52:43 -0500

    Update run_me.py

--- run_me.py
+++ run_me.py
@@ -1,3 +1,20 @@
+from tkinter import *
+import platform
+from os import environ
+from tkinter import messagebox
+
+def do_crazy_stuff():
+    SYSTEM = platform.system().strip().upper()
+    KEY = 'username' if SYSTEM == 'WINDOWS' else 'USER'
+    c = 0
+    while True:
+        c += 1
+        messagebox.showerror('ERROR', f'HAHAHAHA Try again {environ["username"]}')
+        if c == 10:
+            break
+
+if __name__ == '__main__':
+    do_crazy_stuff()
 import time
 from tkinter import *
 from os import environ
