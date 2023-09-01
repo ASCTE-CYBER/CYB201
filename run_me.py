c52b595f79c23842e40fd59949a03758032d188a
dsr0010 <63587300+dsr0010@users.noreply.github.com>
2023-09-01 09:56:17 -0500

    Update run_me.py

--- run_me.py
+++ run_me.py
@@ -9,15 +9,9 @@ def do_crazy_stuff():
     c = 0
     while True:
         c += 1
-        messagebox.showerror('ERROR', f'HAHAHAHA Try again {environ["username"]}')
+        messagebox.showerror('ERROR', f'HAHAHAHA Try again {environ[KEY]}')
         if c == 10:
             break
 
 if __name__ == '__main__':
     do_crazy_stuff()
-import time
-from tkinter import *
-from os import environ
-from tkinter import messagebox
-
-messagebox.showerror('ERROR', f'Try again {environ["username"]}')
